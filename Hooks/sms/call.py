a8c3dca259f63ff02274bf7f4c83cd097debab52
Chris <chrismobberley@gmail.com>
2017-10-17 05:23:51 +0100

    Update call.py
    Changes to call for new twilio python library

--- Hooks/sms/call.py
+++ Hooks/sms/call.py
@@ -9,28 +9,28 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
- 
+
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
- 
+
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
- 
+
 # Module by Martin Stone
 
 from Core.config import Config
 from Core.maps import User
 from Core.loadable import loadable, route, require_user
 if Config.get("Twilio", "sid"):
-    from twilio.rest import TwilioRestClient
+    from twilio.rest import Client
 
 class call(loadable):
     """Calls the user's phone."""
     usage = " <nick>"
-    
+
     @route(r"(\S+)", access = "member")
     @require_user
     def execute(self, message, user, params):
@@ -45,7 +45,7 @@ class call(loadable):
             return
         if receiver.smsmode == "Retard":
             message.reply("I refuse to talk to that incompetent retard. Check %s's mydef comment and use !phone show to try sending it using your own phone." %(receiver.name,))
-            return 
+            return
 
         if not (receiver.pubphone or user in receiver.phonefriends or user.is_admin()):
             message.reply("%s's phone number is private or they have not chosen to share their number with you. No call made." % (receiver.name,))
@@ -56,19 +56,19 @@ class call(loadable):
             message.reply("%s has no phone number or their phone number is too short to be valid (under 6 digits). No call made." % (receiver.name,))
             return
 
-        client = TwilioRestClient(Config.get("Twilio", "sid"), Config.get("Twilio", "auth_token"))
+        client = Client(Config.get("Twilio", "sid"), Config.get("Twilio", "auth_token"))
         if Config.getboolean("Twilio", "warn"):
             url="http://twimlets.com/echo?Twiml=%3CResponse%3E%3CSay%20voice%3D%22alice%22%20language%3D%22en-GB%22%20%3EHello.%20This%20is%20" +\
                 Config.get("Connection", "nick") + ".%20Stop%20wasting%20our%20credit!%3C%2FSay%3E%3CHangup%2F%3E%3C%2FResponse%3E&",
         else:
             url="http://twimlets.com/echo?Twiml=%3CResponse%3E%3CHangup%2F%3E%3C%2FResponse%3E&",
-        tw = client.calls.create(to=phone, from_=Config.get("Twilio", "number"), url=url, Timeout=Config.getint("Twilio", "timeout"))
+        call = client.api.account.calls.create(to=phone, from_=Config.get("Twilio", "number"), url=url, timeout=Config.getint("Twilio", "timeout"))
 
-        if tw.sid:
+        if call.sid:
             message.reply("Successfully called %s." % (receiver.name))
         else:
             message.reply("Error: Failed to get call ID from Twilio server.")
-        
+
     def prepare_phone_number(self,text):
         if not text:
             return text
